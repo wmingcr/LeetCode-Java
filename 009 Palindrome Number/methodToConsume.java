3689ce80f625459e300e7c73003aea2c9e4c8da2
001045854_Ming <wang.ming1@husky.neu.edu>
2020-02-05 18:54:42 -0800

    Update methodToConsume.java

--- 009 Palindrome Number/methodToConsume.java
+++ 009 Palindrome Number/methodToConsume.java
@@ -1,3 +1,17 @@
+/*
+举个例子：1221 这个数字。
+
+通过计算 1221 / 1000， 得首位1
+通过计算 1221 % 10， 可得末位 1
+进行比较
+再将 22 取出来继续比较
+
+作者：MisterBooo
+链接：https://leetcode-cn.com/problems/palindrome-number/solution/dong-hua-hui-wen-shu-de-san-chong-jie-fa-fa-jie-ch/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
 class Solution {
     public boolean isPalindrome(int x) {
         //边界判断
@@ -15,8 +29,9 @@ class Solution {
         return true;
     }
 }
-
+/*
 作者：MisterBooo
 链接：https://leetcode-cn.com/problems/palindrome-number/solution/dong-hua-hui-wen-shu-de-san-chong-jie-fa-fa-jie-ch/
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+/*
