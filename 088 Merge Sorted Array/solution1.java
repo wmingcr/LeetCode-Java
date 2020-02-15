0afc9e5009433d5b9c2ed8d40f46165c713c64c8
001045854_Ming <wang.ming1@husky.neu.edu>
2020-02-15 00:16:50 -0800

    Update solution1.java

--- 088 Merge Sorted Array/solution1.java
+++ 088 Merge Sorted Array/solution1.java
@@ -1,1 +1,22 @@
-
+//last part of Merge Sort,
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        for( ; k >= 0 && i >= 0 && j >= 0; ){
+          //choose the bigger one,
+            if(nums1[i] >= nums2[j]){
+                nums1[k--] = nums1[i--];
+            }else{
+                nums1[k--] = nums2[j--];
+            }
+        }
+      //check any elements left here,
+        while(i>=0){
+            nums1[k--] = nums1[i--];
+        }
+      //check any elements left here,
+        while(j>=0){
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
