b664a88a747a3f0158ae8e2d7e01a5cec5463b57
001045854_Ming <wang.ming1@husky.neu.edu>
2020-02-14 23:13:05 -0800

    Create solution2.java

---
+++ 066 Plus One/solution2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        //if no elements,
+        if(digits.length == 0){
+            return new int [0];
+        }
+        for(int i = digits.length - 1; i >= 0; i--){
+            if(digits[i] != 9){//if not 9, then plus 1
+                digits[i] += 1;
+                return digits; 
+            }else{//in this case, digits[i] = 9;
+                digits[i] = 0;
+            }
+        }
+        //all elements are 9
+        int [] te = new int [digits.length + 1];
+        te[0] = 1;
+        return te;
+    }
+}
