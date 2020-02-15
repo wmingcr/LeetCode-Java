19b6e9b99c00bd0c9e50f9da9a20f087a3f0d36f
001045854_Ming <wang.ming1@husky.neu.edu>
2020-02-14 23:16:06 -0800

    Create solution3_trickyOne.java

---
+++ 066 Plus One/solution3_trickyOne.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        //if no elements,
+        if(digits.length == 0){
+            return new int [0];
+        }
+        for(int i = digits.length - 1; i >= 0; i--){
+            //no matter what, adding one first, then check whether it's 0 or not
+            //very good*******************
+            digits[i]++;
+            digits[i] %= 10;
+            if(digits[i] != 0){
+                return digits;
+            }
+        }
+        //all elements are 9
+        int [] te = new int [digits.length + 1];
+        te[0] = 1;
+        return te;
+    }
+}
