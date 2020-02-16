1c642bfefe4b14d544340e853a1b5fc6efbb86f0
001045854_Ming <wang.ming1@husky.neu.edu>
2020-02-15 21:09:39 -0800

    Update solution1_NestedForLoop.java

--- 119 Pascal's Triangle II/solution1_NestedForLoop.java
+++ 119 Pascal's Triangle II/solution1_NestedForLoop.java
@@ -1,20 +1,27 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
+        //for results
         List<Integer> res = new ArrayList<>();
+        //check validity of rowIndex
         if(rowIndex < 0){
             return res;
         }
         res.add(1);
+        //if it's 0, return it here,
         if(rowIndex == 0){
             return res;
         }
         for(int i = 0; i < rowIndex; i++){
+            //a temp List to store values
             List<Integer> tem = new ArrayList<>();
             tem.add(1);
             for(int j = 0; j < i; j++){
+                //change the values, according to the triangle
                 tem.add(res.get(j) + res.get(j+1));
             }
+            //adding last element,
             tem.add(1);
+            //pass it to result,
             res = tem;
         }
         return res;
