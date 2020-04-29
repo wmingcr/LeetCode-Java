86aadb4b89fcac80948deacefbe791265fc861b4
Ming <wang.ming1@husky.neu.edu>
2020-04-28 17:29:46 -0700

    Update Solution3.java

--- 027 Remove Element/Solution3.java
+++ 027 Remove Element/Solution3.java
@@ -1,8 +1,30 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         //return twoPointers1(nums, val);
-        return twoPointers2(nums, val);
+        //return twoPointers2(nums, val);
+        return twoPointers3(nums, val);
     }
+    
+    /*
+    the second method go through the array 2n times
+    however, we can use this one to reduce it to n times
+    */
+    private int twoPointers3(int [] nums, int val){
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int len = 0; 
+        for(int i = 0, j = nums.length - 1; i <= j; ){
+            if(nums[i] == val){
+                nums[i] = nums[j];
+                j--;
+            }else{
+                len++;i++;
+            }
+        }
+        return len;
+    }
+    
     
     /*
     for twoPointers1, if array is like no element equals val, there will be 
