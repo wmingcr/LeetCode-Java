c4755d9ce4428959ee05b6545a11dc9bf45d43d3
Ming <wang.ming1@northeastern.edu>
2020-04-30 13:19:23 -0700

    Update Solutions_BSTheBestOne.java

--- 011 Container with Most water/Solutions_BSTheBestOne.java
+++ 011 Container with Most water/Solutions_BSTheBestOne.java
@@ -27,3 +27,32 @@ class Solution {
         return mostWater;
     }
 }
+
+
+//-------The Best Submission 
+//Here height[i] is the height of wall and 2 contigious heights (height[i], height[i+1] are separated by 1 unit)
+class Solution {
+    public int maxArea(int[] height) {
+		//we start with 2 pointers at the 2 ends assuming that the width between the 2 walls/heights being the maximum, area could be maximum.
+		//Note that the area in this case would be width * (shorter wall height)
+		//We then shrink the shorter height/wall closer and recalculate area and check if this is more than maxArea
+        int start=0, end=height.length-1;
+        int maxArea = 0;
+        while(start<end) {
+            int area = Math.min(height[start], height[end]) * (end-start);
+            maxArea = Math.max(maxArea,area);
+            //System.out.println("start = "+start+" End = "+end+" area = "+area+" maxArea = "+ maxArea);
+            if(height[start] > height[end] && start<end) {
+                int currHeight = height[end];
+                while(height[--end]<=currHeight) ;
+                //end--;
+            }
+            else {
+                int currHeight = height[start];
+                while(height[++start] <= currHeight && start<end) ;
+                //start++;
+            }
+        }
+        return maxArea;
+    }
+}
