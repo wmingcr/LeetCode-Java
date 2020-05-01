62bbab6686330192a18b89ecd836a2c98bee4a66
Ming <wang.ming1@northeastern.edu>
2020-05-01 15:16:19 -0700

    Update Solutions_排列组合.java

--- 1395 Count number of Teams/Solutions_排列组合.java
+++ 1395 Count number of Teams/Solutions_排列组合.java
@@ -15,7 +15,23 @@ class Solution {
         for(int i = 1; i < rating.length-1; i ++){
             //进行排列组合
             int LLess = 0, LMore = 0, RMore = 0, RLess = 0;
-            for(int k = 0; k < rating.length; k++){
+            
+            //optimization
+            for(int k = 0; k < i; k ++){
+                if(rating[k] < rating[i]){
+                    LLess++;
+                }else if (rating[k] > rating[i]){
+                    LMore++;
+                }
+            }
+            for(int k = i+1; k < rating.length; k++){
+                if(rating[k] < rating[i]){
+                    RLess++;
+                }else if (rating[k] > rating[i]){
+                    RMore++;
+                }
+            }
+            /**for(int k = 0; k < rating.length; k++){
                 if(k < i && rating[k] < rating[i]){
                     LLess++;
                 }else if (k < i && rating[k] > rating[i]){
@@ -26,6 +42,7 @@ class Solution {
                     RMore++;
                 }
             }
+            */
             ans += LLess*RMore + LMore*RLess;
         }
         return ans;
