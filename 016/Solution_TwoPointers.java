8b7167a132a21c14e036101b129a272afb0c89cb
Ming <wang.ming1@northeastern.edu>
2020-04-30 17:58:16 -0700

    Create Solution_TwoPointers.java

---
+++ 016/Solution_TwoPointers.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        //validity
+        if(nums == null || nums.length < 3){
+            return -1;
+        }
+        Arrays.sort(nums);
+        int ans = nums[0] + nums[1] + nums[2];
+        for(int i = 0; i < nums.length; i ++){
+            int j = i+1, k = nums.length-1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(Math.abs(target - sum) < Math.abs(target - ans)){
+                    ans = sum;
+                }
+                if(sum > target){
+                    k--;
+                }else if(sum < target){
+                    j++;
+                }else{
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
