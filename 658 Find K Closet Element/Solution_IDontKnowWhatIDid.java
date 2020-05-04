f2a5dd9ab51b53df165170aa169bad2027aacadf
Ming <wang.ming1@northeastern.edu>
2020-05-04 01:25:13 -0700

    Update Solution_IDontKnowWhatIDid.java

--- 658 Find K Closet Element/Solution_IDontKnowWhatIDid.java
+++ 658 Find K Closet Element/Solution_IDontKnowWhatIDid.java
@@ -7,10 +7,12 @@ class Solution {
             return new LinkedList<Integer>();
         }
         
+        //initializint answer
         List<Integer> ans = new LinkedList<>();
         
         //corner case, x is less or equal than the first element
         if(x <= arr[0]){
+            
             int idx = 0, te = Math.min(k, arr.length);
             while(idx < te){
                 ans.add(arr[idx++]);
@@ -22,9 +24,10 @@ class Solution {
         
         //coner case, x is greater or equal than the last element
         if(x >= arr[n]){
+            
             int te = arr.length - k;
             
-            if(te < 0){
+            if(te < 0){//from the first element
                 te = 0;
             }
             
@@ -34,12 +37,14 @@ class Solution {
             return ans;
         }
         
+        //find the element which is the closest to x
         int idx_Closest = findClosestIdx(arr, x);
         
+        //find the first element to be returned
         int leftBound = findLeftBound(arr, k, x, idx_Closest);
         
+        //storing k elements
         int newIdx = 0;
-        
         while(newIdx++ < k){
             ans.add(arr[leftBound++]);
         }
@@ -47,45 +52,57 @@ class Solution {
         return ans;
     }
     
+    //find the first element to be stored in List
     private int findLeftBound(int[] arr, int k, int x, int idx){
+        
+        //while we already found idx, so te can just start from 1
         int te = 1, left = idx, right = idx;
         
         while(te < k){
             int leftABS = x - arr[left-1];
             int rightABS = x - arr[right+1];
             
-            if(Math.abs(leftABS) <= Math.abs(rightABS)){
+            if(Math.abs(leftABS) <= Math.abs(rightABS)){ // with "=", get the left element
                 left--;
             }else{
                 right++;
             }
-            te++;
-            if(left == 0){
-                return 0;
+            
+            te++; // increasing by 1
+            
+            if(left == 0){ //incase out of left bound
+                return 0; 
             }
-            if(right == arr.length-1){
+            if(right == arr.length-1){ // incase out of right bound
                 return arr.length - k;
             }
         }
         return left;
     }
     
+    //find the element which is the closest one to x
     private int findClosestIdx(int[] arr, int x){
+        //idx, index of element closest to x
         int idx = 0, abs = Integer.MAX_VALUE;
+        
+        //initialization for binary search
         int left = 0, right = arr.length-1;
         
+        //binary search
         while(left <= right){
             
+            //mid pointer
             int mid = left + ((right-left)>>1);
             
+            //got a new abs
             int newAbs = Math.abs(x - arr[mid]);
             
             if(newAbs < abs){
-                idx = mid;
+                idx = mid; // renew index
             }
             
             if(arr[mid] == x){
-                return idx;
+                return idx; // ending condition
             }
             
             if(arr[mid] > x){
@@ -97,3 +114,4 @@ class Solution {
         return idx;
     }
 }
+
