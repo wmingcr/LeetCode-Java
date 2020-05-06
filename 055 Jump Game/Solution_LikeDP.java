612dfe12e0133fab61cebe395acd25d2b3c48c89
Ming <wang.ming1@northeastern.edu>
2020-05-06 15:37:52 -0700

    Update Solution_LikeDP.java

--- 055 Jump Game/Solution_LikeDP.java
+++ 055 Jump Game/Solution_LikeDP.java
@@ -5,20 +5,26 @@ class Solution {
             return false;
         }
         
+        //the max index we can reach
         int maxJump = 0;
         
+        //traverse all index
         for(int i = 0; i < nums.length; i ++){
             
+            //if maxJump cant reach current i
             if(maxJump < i){
                 return false;
             }
             
             maxJump = Math.max(maxJump, nums[i]+i);
             
+            //if maxJupm is greater or equal than last element, (can reach last element)
             if(maxJump >= nums.length-1){
                 return true;
             }
         }
+        
+        //doesnt matter what we will return here
         return false;
     }
 }
