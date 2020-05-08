de896644a9ab857fc50681443df7a924c72e0213
Ming <wang.ming1@northeastern.edu>
2020-05-08 02:05:54 -0700

    Update Solution_Interesting.java

--- 453 Minimun Moves to Equal Array Elements/Solution_Interesting.java
+++ 453 Minimun Moves to Equal Array Elements/Solution_Interesting.java
@@ -1,3 +1,5 @@
+//https://leetcode-cn.com/problems/minimum-moves-to-equal-array-elements/solution/zui-xiao-yi-dong-ci-shu-shi-shu-zu-yuan-su-xiang-d/
+
 class Solution {
     public int minMoves(int[] nums) {
         //validity
