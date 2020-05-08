8c53efded81e9aea1fa71c0e688b13df4dcdfbae
Ming <wang.ming1@northeastern.edu>
2020-05-08 02:30:38 -0700

    Create Solution_MathOptimization.java

---
+++ 453 Minimum Moves to Equal Array Elements/Solution_MathOptimization.java
@@ -0,0 +1,26 @@
+//https://leetcode-cn.com/problems/minimum-moves-to-equal-array-elements/solution/zui-xiao-yi-dong-ci-shu-shi-shu-zu-yuan-su-xiang-d/
+
+class Solution {
+    public int minMoves(int[] nums) {
+        //validity
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+        
+        int min = nums[0];
+        
+        for(int i = 0; i < nums.length; i ++){
+            if(nums[i] < min){
+                min = nums[i];
+            }
+        }
+        
+        int step = 0; 
+        
+        for(int i = 0; i < nums.length; i ++){
+            step += nums[i] - min;
+        }
+        
+        return step;
+    }
+}
